07636dae9edbab3b59c1c571088a9d2dd1079571
Heo Jong Tae <heo9910@naver.com>
2020-11-23 20:29:05 +0900

    .

--- src/main/java/com/study/thymeleaf/config/LocaleConfiguration.java
+++ src/main/java/com/study/thymeleaf/config/LocaleConfiguration.java
@@ -4,7 +4,6 @@ import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
