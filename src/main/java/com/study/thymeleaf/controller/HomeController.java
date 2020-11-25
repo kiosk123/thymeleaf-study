cbeef00132f2ae5673f397e8729433f54bc0c34a
Heo Jong Tae <heo9910@naver.com>
2020-11-25 16:05:37 +0900

    .

--- src/main/java/com/study/thymeleaf/controller/HomeController.java
+++ src/main/java/com/study/thymeleaf/controller/HomeController.java
@@ -1,14 +1,19 @@
 package com.study.thymeleaf.controller;
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.study.thymeleaf.dto.UserDTO;
+
 @Controller
 public class HomeController {
     @GetMapping({"/", "/index"})
-    public String index(Model model) {
+    public String index(HttpSession session, Model model) {
+        session.setAttribute("user", new UserDTO("unknown"));
         model.addAttribute("welcome", "welcome my page!");
         return "index";
     }
---
+++ src/main/java/com/study/thymeleaf/dto/UserDTO.java
@@ -0,0 +1,10 @@
+package com.study.thymeleaf.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDTO {
+    private String userName;
+}
