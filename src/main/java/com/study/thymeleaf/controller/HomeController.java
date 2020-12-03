60d1210d936e5488718f90119cb2531dc6a31068
Heo Jong Tae <heo9910@naver.com>
2020-12-03 16:32:46 +0900

    .

--- src/main/java/com/study/thymeleaf/controller/HomeController.java
+++ src/main/java/com/study/thymeleaf/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.study.thymeleaf.controller;
 
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 import javax.servlet.http.HttpSession;
 
@@ -18,6 +21,13 @@ public class HomeController {
         session.setAttribute("user", new UserDTO("unknown"));
         model.addAttribute("welcome", "welcome my page!");
         model.addAttribute("today", LocalDateTime.now());
+        
+        List<UserDTO> list = new ArrayList<>();
+        IntStream.range(0, 10).forEach(n -> {
+            UserDTO dto = new UserDTO("user" + n);
+            list.add(dto);
+        });
+        model.addAttribute("list", list);
         return "index";
     }
 }
